fc35abdce92d8583c6e59fa89d521b16330b9804
Gregor Gabriel <gregor.gabriel@unit.no>
2021-03-29 16:56:11 +0200

    get test coverage up again. more

--- src/test/java/no/unit/bibs/contents/UpdateContentsApiHandlerTest.java
+++ src/test/java/no/unit/bibs/contents/UpdateContentsApiHandlerTest.java
@@ -3,6 +3,7 @@ package no.unit.bibs.contents;
 import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import no.unit.bibs.contents.exception.ParameterException;
 import nva.commons.exceptions.ApiGatewayException;
 import nva.commons.handlers.RequestInfo;
 import nva.commons.utils.Environment;
@@ -16,7 +17,7 @@ import java.nio.file.Path;
 import static no.unit.bibs.contents.DynamoDBClientTest.SAMPLE_TERM;
 import static nva.commons.handlers.ApiGatewayHandler.ALLOWED_ORIGIN_ENV;
 import static nva.commons.utils.JsonUtils.objectMapper;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -24,6 +25,8 @@ class UpdateContentsApiHandlerTest {
 
     private Environment environment;
     private Table dynamoTable;
+    private DynamoDBClient dynamoDBClient;
+    private UpdateContentsApiHandler handler;
 
     public static final String CREATE_CONTENTS_EVENT = "createContentsEvent.json";
 
@@ -31,9 +34,12 @@ class UpdateContentsApiHandlerTest {
      * javadoc for checkstyle.
      */
     @BeforeEach
-    private void initEnvironment() {
+    public void init() {
         environment = mock(Environment.class);
+        when(environment.readEnv(ALLOWED_ORIGIN_ENV)).thenReturn("*");
         dynamoTable = mock(Table.class);
+        dynamoDBClient = mock(DynamoDBClient.class);
+        handler = new UpdateContentsApiHandler(environment, dynamoDBClient);
     }
 
     @Test
@@ -57,6 +63,16 @@ class UpdateContentsApiHandlerTest {
         GatewayResponse response =  new GatewayResponse(environment, SAMPLE_TERM, HttpStatus.SC_OK);
         Integer statusCode = handler.getSuccessStatusCode(null, response);
         assertEquals(statusCode, HttpStatus.SC_OK);
+    }
+
+
+    @Test
+    void handlerThrowsExceptionWithEmptyRequest()  {
+        Exception exception = assertThrows(ParameterException.class, () -> {
+            handler.processInput(null, new RequestInfo(), mock(Context.class));
+        });
+
+        assertTrue(exception.getMessage().contains(UpdateContentsApiHandler.NO_PARAMETERS_GIVEN_TO_HANDLER));
     }
 
 }
