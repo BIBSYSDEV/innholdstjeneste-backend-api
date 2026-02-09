394413367cb6901540aace91734b71edb6e70e8c
Tor Arne Torgersen <6398417+torarnet@users.noreply.github.com>
2026-02-09 16:29:15 +0100

    ts-138: more test coverage

--- src/test/java/no/unit/bibs/contents/UpdateContentsApiHandlerTest.java
+++ src/test/java/no/unit/bibs/contents/UpdateContentsApiHandlerTest.java
@@ -6,9 +6,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import no.unit.bibs.contents.exception.CommunicationException;
 import no.unit.nva.commons.json.JsonUtils;
 import no.unit.nva.testutils.HandlerRequestBuilder;
 import nva.commons.apigateway.GatewayResponse;
+import nva.commons.apigateway.exceptions.NotFoundException;
 import nva.commons.core.Environment;
 import nva.commons.core.ioutils.IoUtils;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,9 +19,11 @@ import org.junit.jupiter.api.Test;
 import java.nio.file.Path;
 
 import static java.net.HttpURLConnection.HTTP_BAD_REQUEST;
+import static java.net.HttpURLConnection.HTTP_CONFLICT;
 import static java.net.HttpURLConnection.HTTP_CREATED;
 import static no.unit.bibs.contents.ContentsRequest.DOCUMENT_JSON_NOT_VALID;
 import static no.unit.bibs.contents.ContentsRequest.MALFORMED_JSON_PAYLOAD;
+import static no.unit.bibs.contents.UpdateContentsApiHandler.FAILED_AFTER_PERSISTING;
 import static no.unit.bibs.contents.UpdateContentsApiHandler.NO_PARAMETERS_GIVEN_TO_HANDLER;
 import static no.unit.nva.commons.json.JsonUtils.dtoObjectMapper;
 import static nva.commons.apigateway.ApiGatewayHandler.ALLOWED_ORIGIN_ENV;
@@ -30,6 +34,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -61,7 +66,7 @@ class UpdateContentsApiHandlerTest {
     }
 
     @Test
-    void shouldCreateContentsRequestResourceFromInputAndReturnHttpCodeCreatedWhenNotFoundFromBefore()
+    void shouldCreateContentsRequestResourceFromInputAndReturnHttpCodeCreatedWhenGetContentFromResourceReturnsEmpty()
         throws Exception {
 
         var contents = getContentsString();
@@ -78,6 +83,43 @@ class UpdateContentsApiHandlerTest {
         assertThat(responseBody, equalTo(contentsDocument));
         assertThat(responseBody.getIsbn(), equalTo(contentsDocument.getIsbn()));
         assertThat(responseBody.getDescriptionLong(), equalTo(contentsDocument.getDescriptionLong()));
+    }
+
+    @Test
+    void shouldCreateContentsRequestResourceFromInputWhenGetContentsThrowNotFoundExceptionIndicatingNotAlreadyExists()
+        throws Exception {
+
+        var contents = getContentsString();
+        var contentsDocument = getContentsDocument(contents);
+        var contentsRequest = new ContentsRequest(contentsDocument);
+
+        doNothing().when(dbClient).createContents(contentsDocument);
+        when(dbClient.getContents(anyString())).thenThrow(NotFoundException.class).thenReturn(contents);
+
+        var response = sendQuery(contentsRequest);
+        var responseBody = response.getBodyObject(ContentsDocument.class);
+
+        assertThat(response.getStatusCode(), equalTo(HTTP_CREATED));
+        assertThat(responseBody, equalTo(contentsDocument));
+        assertThat(responseBody.getIsbn(), equalTo(contentsDocument.getIsbn()));
+        assertThat(responseBody.getDescriptionLong(), equalTo(contentsDocument.getDescriptionLong()));
+    }
+
+    @Test
+    void shouldThrowConflictExceptionWhenCreateContentsRequestFromInputCouldNotBePersisted()
+        throws Exception {
+
+        var contents = getContentsString();
+        var contentsDocument = getContentsDocument(contents);
+        var contentsRequest = new ContentsRequest(contentsDocument);
+
+        doThrow(CommunicationException.class).when(dbClient).updateContents(contentsDocument);
+        when(dbClient.getContents(anyString())).thenReturn(contents);
+
+        var response = sendQuery(contentsRequest);
+
+        assertThat(response.getStatusCode(), equalTo(HTTP_CONFLICT));
+        assertThat(response.getBody(), containsString(FAILED_AFTER_PERSISTING));
     }
 
     @Test
